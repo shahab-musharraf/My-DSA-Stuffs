1d015f32350d2946a313a3458c7bb232c6e35b62
Shahab Musharraf <112477232+shahab-musharraf@users.noreply.github.com>
2022-12-03 14:33:52 +0530

    Create Day1_2-PrimeNumber.cpp

---
+++ Day1_2-PrimeNumber.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+  int n;
+  cin>>n;
+  for(int i=2; i<n; i++){
+    if(n%i==0){
+      cout<<"Not a Prime Number";
+      return 0;
+    }
+  }
+  cout<<"Prime Number";
+  return 0;
+}
