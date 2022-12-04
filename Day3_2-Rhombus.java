7f496169fbfdfddd496883d782f600f6252733ca
Shahab Musharraf <112477232+shahab-musharraf@users.noreply.github.com>
2022-12-04 15:24:10 +0530

    Create Day3_2-Rhombus.java

---
+++ Day3_2-Rhombus.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class Main{
+    public static void main(String args []){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        
+        for(int i=1 ;i<=n; i++){
+            int x=n-i;
+            for(int j=1; j<=x;j++){
+                System.out.print(" ");
+            }
+            for(int j=1; j<=n;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
