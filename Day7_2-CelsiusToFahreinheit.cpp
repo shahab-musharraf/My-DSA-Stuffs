f759101196a8ae4077293004e18a7a29a15dc4f8
Shahab Musharraf <112477232+shahab-musharraf@users.noreply.github.com>
2022-12-08 23:59:20 +0530

    Create Day7_2-CelsiusToFahreinheit.cpp

---
+++ Day7_2-CelsiusToFahreinheit.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+  float cel;
+  cin>>cel;
+  
+  float fah=(cel*1.8)+32;
+  cout<<cel<<" degree celsius is equal to "<<fah<<" degree fahreinheit"<<endl; 
+  return 0;
+}
